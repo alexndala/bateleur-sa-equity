295d520ab9cb6bacfe9c4b39bc425d5b2830543f
Mahlatse Alex Ndala <axndala@gmail.com>
2024-12-15 21:56:09 +0200

    ran the 2020 crash simulation

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import riskfolio as rp
 
 
-ax = rp.plot_clusters(returns = returns)
+asset_classes = pd.read_csv("asset_bateleur30.csv")
+
+asset_classes = pd.DataFrame(asset_classes)
+asset_classes = asset_classes.sort_values(by=['Assets'])
+asset_classes.reset_index(inplace=True, drop=True)
+
+# Risk contribution constraints vector based on Industry Class
+b = rp.risk_constraint(asset_classes,
+                       kind='classes',
+                       classes_col='Industry')
+print(b)
