09207402731d67a12d42c1a4707df0d4357d3042
Mahlatse Alex Ndala <axndala@gmail.com>
2024-12-22 02:05:49 +0200

    created the normal index tracking and the equal risk contribution vector based on industry asset type. do the final touches.

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import riskfolio as rp
 
-# Building the portfolio object
+import matplotlib.pyplot as plt
 
-port = rp.Portfolio(returns=Y[assets])
-# Calculating optimal portfolio
+ax = rp.jupyter_report(returns, w=w_rp, rm=rm, t_factor = 252, days_per_year = 365)
+plt.show()
 
-# Select method and estimate input parameters:
-
-method_mu='hist' # Method to estimate expected returns based on historical data.
-method_cov='hist' # Method to estimate covariance matrix based on historical data.
+# Risk contribution constraints vector based on Industry Class
+b = rp.risk_constraint(asset_classes,
+                       kind='classes',
+                       classes_col='Industry')
