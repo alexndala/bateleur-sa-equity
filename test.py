fa004895d8c868ef9b47682a95383a970cc0ac84
Mahlatse Alex Ndala <axndala@gmail.com>
2025-01-07 00:43:18 +0200

    vanilla assets

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ plt.show()
 b = rp.risk_constraint(asset_classes,
                        kind='classes',
                        classes_col='Industry')
+
+ax = rp.plot_clusters(returns = returns,
+                    codependence="pearson",
+                    linkage="ward",
+                    cmap="viridis",
+                    linecolor="fuchsia",
+                    title="Assets Clustermap (Pearson and ward linkage)\n Analyst: Mahlatse A. Ndala.",
+                
+                    height=12,
+                    width=12)
