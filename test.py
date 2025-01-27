03fca99b6642129390b752c56942f19fb2860212
Mahlatse Alex Ndala <axndala@gmail.com>
2025-01-27 21:12:56 +0200

    demonstrated the differnce between spearman and pearson

--- test.py
+++ test.py
@@ -5,17 +5,15 @@ import matplotlib.pyplot as plt
 ax = rp.jupyter_report(returns, w=w_rp, rm=rm, t_factor = 252, days_per_year = 365)
 plt.show()
 
-# Risk contribution constraints vector based on Industry Class
-b = rp.risk_constraint(asset_classes,
-                       kind='classes',
-                       classes_col='Industry')
-
+# # Risk contribution constraints vector based on Industry Class
+# b = rp.risk_constraint(asset_classes,
+#                        kind='classes',
+#                        classes_col='Industry')
 ax = rp.plot_clusters(returns = returns,
-                    codependence="pearson",
+                    codependence="spearman",
                     linkage="ward",
-                    cmap="viridis",
-                    linecolor="fuchsia",
-                    title="Assets Clustermap (Pearson and ward linkage)\n Analyst: Mahlatse A. Ndala.",
-                
-                    height=12,
+                    cmap="BrBG",
+                    linecolor="blue",
+                    title="XYZ BCI SA Equity Fund Clustermap (Spearman and Ward) \nJan-2024 - Dec-24\n Analyst: Mahlatse A. Ndala.",               
+                    height=10,
                     width=12)
